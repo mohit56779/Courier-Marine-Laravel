1a6ef5ab8ad83b8cc7882915479d926671b9f614
Mohit <mohit56779@gmail.com>
2021-07-13 13:00:12 -0600

    logout implemented in dashboard

--- resources/views/customer/layouts/inc/navbar.blade.php
+++ resources/views/customer/layouts/inc/navbar.blade.php
@@ -13,38 +13,17 @@
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-      <!-- Navbar Search -->
+      <!-- Logout Button -->
       <li class="nav-item">
-        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-          <i class="fas fa-search"></i>
-        </a>
-        <div class="navbar-search-block">
-          <form class="form-inline">
-            <div class="input-group input-group-sm">
-              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-              <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+        <a href="{{route('logout')}}" class="text-danger" onclick="event.preventDefault();document.getElementById('logout-form').submit();"> Logout </a>
+
+        <form action="{{route('logout')}}" id="logout-form" method="POST" style="display:none;">
+          @csrf
+        </form>
+        
       </li>
 
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
+
     </ul>
   </nav>
   <!-- /.navbar -->
--- resources/views/customer/layouts/inc/sidebar.blade.php
+++ resources/views/customer/layouts/inc/sidebar.blade.php
@@ -1,10 +1,10 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
+    <div class="brand-link">
       <img src="{{ asset('admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+      <span class="brand-text font-weight-light">Courier Marine</span>
+    </div>
 
     <!-- Sidebar -->
     <div class="sidebar">
